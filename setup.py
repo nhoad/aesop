cba7521fd0b841e8d404ac77f1a77609f88da37b
Nathan Hoad <nathan@getoffmalawn.com>
2015-06-01 20:16:03 +1000

    Tiny setup.py for deploying.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aesop',
+      version='0.1',
+      description='aesop media player',
+      author='Nathan Hoad',
+      author_email='nathan@getoffmalawn.com',
+      packages=['aesop'],
+     )
